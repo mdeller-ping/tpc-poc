ae30888b0f5b6aaef61b308666790796a8e4092a
mdeller-ping <mdeller@pingidentity.com>
2019-12-09 14:40:55 -0500

    disable ssl logging

--- pingfederate/instance/bin/run.sh
+++ pingfederate/instance/bin/run.sh
@@ -10,7 +10,7 @@
 DIRNAME=`dirname "$0"`
 PROGNAME=`basename "$0"`
 GREP="grep"
-JAVA_OPTS="-Djavax.net.debug=ssl,handshake"
+# JAVA_OPTS="-Djavax.net.debug=ssl,handshake"
 
 # Use the maximum available, or set MAX_FD != -1 to use that
 MAX_FD="maximum"
